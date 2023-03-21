50f60e0dbbdca2f104cd75f7216afce52eb32f8b
Garrett Dancik <garrett.dancikg@gmail.com>
2023-03-21 10:48:09 -0400

    Ready for Spring 2023

---
+++ hello.java
@@ -0,0 +1,14 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+       // initialize variables
+       String firstName = "Alex";
+	
+       // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
+
+    }
+}
+
